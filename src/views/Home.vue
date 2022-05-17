fb6e258647854339aa8857307f9a4cc185104265
John Chichester <jchichester7@gmail.com>
2022-05-16 21:02:36 -0400

    add api link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,29 @@
 
 <script>
 // @ is an alias to /src
+import axios from "axios";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  data: function () {
+    return {
+      photos: [],
+    };
+  },
+  created: function () {
+    this.indexPhotos();
+  },
+  methods: {
+    indexPhotos: function () {
+      axios.get("https://api.hatchways.io/assessment/students").then((response) => {
+        console.log("photos index", response);
+        this.photos = response.data;
+      });
+    },
   },
 };
 </script>
