fd5e83590a15de21e9f7a03e90010f6f9de36fac
John Chichester <jchichester7@gmail.com>
2022-05-17 11:24:24 -0400

    display facts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="home">
     <div>
-      <ul>
-        BUTTS
-        <li v-for="student in students" v-bind:key="student.id">{{ student }}</li>
-      </ul>
+      <div v-for="student in students" v-bind:key="student.id">
+        <img :scr="student.pic" />
+        <h1>{{ student.firstName + " " + student.lastName }}</h1>
+        <p>Email: {{ student.email }}</p>
+        <p>Company: {{ student.company }}</p>
+        <p>Skill: {{ student.skill }}</p>
+        <p>Average: {{ student.average }}%</p>
+      </div>
     </div>
   </div>
 </template>
+
+<style></style>
 
 <script>
 // @ is an alias to /src
@@ -27,6 +33,11 @@ export default {
       axios.get("https://api.hatchways.io/assessment/students").then((response) => {
         //console.table(response.data.students);
         this.students = response.data.students;
+        //average grades
+        this.students.forEach((student) => {
+          let mean = student.grades.reduce((a, b) => parseInt(a) + parseInt(b), 0) / student.grades.length;
+          student.average = mean;
+        });
       });
     },
   },
