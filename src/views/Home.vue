3ba2f6918954f3eb42443eef5f6e509ec7ab94f3
John Chichester <jchichester7@gmail.com>
2022-05-17 09:48:21 -0400

    got it to display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <ul>
+        BUTTS
+        <li v-for="student in students" v-bind:key="student.id">{{ student }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import axios from "axios";
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
   data: function () {
     return {
       students: [],
